f60ffd45c445341a50b6ba10456d999e82950067
vict0rsch <vsch@protonmail.com>
2018-01-17 18:49:18 +0100

    initial

---
+++ react/src/components/Home/Content/Settings/Stats.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Grid from 'material-ui';
+
+class Stats extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            stats: {}
+        };
+    }
+
+    
+    componentWillMount() {
+        const stats = localStorage.stats
+        this.setState({
+            stats: stats || {};
+        })
+    }
+    
+
+
+    render() {
+        return (
+            <Grid container >
+
+            </Grid>
+        );
+    }
+}
+
+export default Stats;
