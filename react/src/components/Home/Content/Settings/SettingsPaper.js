db138e7bdc51458cc9d9ddf60bb00d0023f2d166
vict0rsch <vsch@protonmail.com>
2018-01-20 14:24:46 +0100

    move style

--- react/src/components/Home/Content/Settings/SettingsPaper.js
+++ react/src/components/Home/Content/Settings/SettingsPaper.js
@@ -6,27 +6,25 @@ import HomePaper from '../../Paper/HomePaper'
 import Stats from './Stats';
 import Grid from 'material-ui/Grid';
 
-const gridStyle = { display: "flex", justifyContent: "center", alignItems: 'center' }
+const gridStyle = { display: "flex", justifyContent: "center", alignItems: 'center', fontSize: '0.7em' }
 
 class SettingsPaper extends Component {
 
     render() {
         return (
-            <div style={{ textAlign: 'center' }}>
-                <HomePaper
-                    {...this.props}
-                    toggle={this.props.toggleSettings}
-                    content={
-                        <Grid container spacing={8}>
-                            <Grid item xs={4} style={gridStyle}><LanguageSelect {...this.props} /></Grid>
-                            <Grid item xs={4} style={gridStyle}><ShowLegend {...this.props} /></Grid>
-                            <Grid item xs={4} style={gridStyle}><ResetApp {...this.props} /></Grid>
-                            <Grid item xs={12} style={gridStyle}></Grid>
-                            <Grid item xs={12} style={gridStyle}><Stats {...this.props} /></Grid>
+            <HomePaper
+                {...this.props}
+                toggle={this.props.toggleSettings}
+                content={
+                    <Grid container spacing={8}>
+                        <Grid item xs={4} style={gridStyle}><LanguageSelect {...this.props} /></Grid>
+                        <Grid item xs={4} style={gridStyle}><ShowLegend {...this.props} /></Grid>
+                        <Grid item xs={4} style={gridStyle}><ResetApp {...this.props} /></Grid>
+                        <Grid item xs={12} style={gridStyle}></Grid>
+                        <Grid item xs={12} style={gridStyle}><Stats {...this.props} /></Grid>
 
-                        </Grid>
-                    } />
-            </div>
+                    </Grid>
+                } />
         );
     }
 }
