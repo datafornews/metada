6ba72282a3b7fcfe9c54cbe2b539de17bb5ccfab
vict0rsch <vsch@protonmail.com>
2018-01-24 22:42:39 +0100

    call to action more visible

--- react/src/components/Graph/InfoBox/InfoBoxEntity.js
+++ react/src/components/Graph/InfoBox/InfoBoxEntity.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Button from 'material-ui/Button';
 import EntityCard from './EntityCard';
-import Timeline from 'material-ui-icons/Timeline';
+import Timeline from 'react-icons/lib/md/more';
 import logGraph from '../../../utils/logGraph';
 
+
+const iconStyle = {
+  height: '30px',
+  width: '30px'
+}
 
 class InfoBoxEntity extends React.Component {
 
@@ -24,7 +29,7 @@ class InfoBoxEntity extends React.Component {
     let graphButton;
     if (entity.category !== 's' && entity.id !== parseInt(this.props.match.params.entityId, 10)) {
       graphButton = (<Button style={{ color: 'green' }} onClick={this.handleClick}>
-        {this.props.translate('graph.seeGraphButton')} &nbsp; &nbsp;<Timeline />
+        {this.props.translate('graph.seeGraphButton')} &nbsp; &nbsp;<Timeline style={iconStyle}/>
       </Button>);
     } else {
       graphButton = undefined;
