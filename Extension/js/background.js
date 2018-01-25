444564faf12cf04d8ddd088923684c191cbbee97
vict0rsch <vsch@protonmail.com>
2018-01-25 12:36:57 +0100

    update extension

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -12,16 +12,30 @@ $(function () {
     console.log('Running Background JS');
 
     window.browser.tabs.onUpdated.addListener(function (onglet) {
+        let logged = false;
         if (!isNaN(onglet)) {
             window.browser.tabs.query({ active: true, lastFocusedWindow: true },
                 function (array_of_tabs) {
                     var tab = array_of_tabs[0];
                     if (tab && sessionStorage['currentTabUrl'] !== tab.url) {
                         sessionStorage['currentTabUrl'] = tab.url;
-                        log_stats(tab)
+                        console.log('Checking for stats');
+                        if (tab.id === onglet
+                            && sessionStorage['tab_' + tab.id + '_previous'] !== tab.url) {
+
+                            logAndCount = true;
+                            logged = true;
+
+                        } else {
+                            logAndCount = false
+                        }
+                        log_stats(tab, logAndCount)
                     }
-                    if (tab && tab.id === onglet
-                        && sessionStorage['tab_' + tab.id + '_previous'] !== tab.url) {
+                    if (tab
+                        && tab.id === onglet
+                        && sessionStorage['tab_' + tab.id + '_previous'] !== tab.url
+                        && !logged) {
+                            
                         sessionStorage['tab_' + tab.id + '_previous'] = tab.url;
                         log_tab(tab);
                         count_tabs()
