2906f0856810d8662a84aa6ed1117874253895cc
vict0rsch <vsch@protonmail.com>
2018-03-25 18:25:37 +0200

    try drawer

---
+++ react/src/components/Drawer/DrawerContent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DrawerContent extends Component {
+    render() {
+        return (
+            <div>
+                Hello my name is drawer content
+            </div>
+        )
+    }
+}
