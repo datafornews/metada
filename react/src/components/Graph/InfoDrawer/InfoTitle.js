777a169ef5866d1dbad9b6db64ae1555d1533dcf
vict0rsch <vsch@protonmail.com>
2018-10-05 12:02:44 +0200

    fix title

--- react/src/components/Graph/InfoDrawer/InfoTitle.js
+++ react/src/components/Graph/InfoDrawer/InfoTitle.js
@@ -61,13 +61,10 @@ class InfoTitle extends Component {
     render() {
         const { classes, infoBox, match, data, translate, clientType } = this.props;
         let entityId = !infoBox.share ? parseInt(infoBox.entity, 10) : parseInt(match.params.entityId, 10);
-        console.log('entityId :', entityId);
         if (!entityId || isNaN(entityId)) {
             entityId = parseInt(match.params.entityId, 10)
         }
         const entity = data.entities.ids[entityId];
-
-        console.log(infoBox);
 
         return (
             <div className={classes.container}>
@@ -82,8 +79,9 @@ class InfoTitle extends Component {
                             {entity.long_name}
                         </Typography>
                         {clientType === "extension" && (
-                            <div><br />
-                                <StatTitle entity={entity} translate={translate} ></StatTitle></div>
+                            <div>
+                                <StatTitle entity={entity} translate={translate} />
+                            </div>
                         )}
 
                     </Grid>
