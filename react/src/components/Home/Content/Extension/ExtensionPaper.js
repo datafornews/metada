f7e2d41a4f16b84fa6903cedea41a52114bb37b7
vict0rsch <vsch@protonmail.com>
2018-10-05 17:56:08 +0200

    add gif

--- react/src/components/Home/Content/Extension/ExtensionPaper.js
+++ react/src/components/Home/Content/Extension/ExtensionPaper.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import MarkdownPaper from '../../Paper/MarkdownPaper';
+
+
+
+const styles = theme => ({
+    container: {
+        textAlign: 'center',
+        margin: 'auto',
+        maxWidth: 550
+    },
+    img: {
+        maxWidth: '100%'
+    }
+});
 
 class ExtensionPaper extends Component {
 
-    
-
     render() {
+        const { classes } = this.props;
         return (
-            <div style={{textAlign:'center'}}>
+            <div style={{ textAlign: 'center' }}>
                 <MarkdownPaper
                     {...this.props}
                     source={this.props.translate('home.extensionPaperMd')}
+                    extra={<div className={classes.container}>
+                        <a href="https://media.giphy.com/media/3dgfdtwTCvQmahTGjW/giphy.gif" target='_blank'>
+                            <img className={classes.img} src='https://media.giphy.com/media/3dgfdtwTCvQmahTGjW/giphy.gif' alt="Loading Metada demonstration gif..."></img>
+                        </a>
+                    </div>}
                     toggle={this.props.toggleContact}
                 />
             </div>
@@ -18,4 +36,4 @@ class ExtensionPaper extends Component {
     }
 }
 
-export default ExtensionPaper;
+export default withStyles(styles)(ExtensionPaper);
