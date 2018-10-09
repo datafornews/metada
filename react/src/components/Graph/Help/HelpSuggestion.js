a07f9e602a539c9545e52d7bc278775d235da214
vict0rsch <vsch@protonmail.com>
2018-10-10 00:15:09 +0200

    improve help ftux

--- react/src/components/Graph/Help/HelpSuggestion.js
+++ react/src/components/Graph/Help/HelpSuggestion.js
@@ -2,27 +2,68 @@ import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 import Button from '@material-ui/core/Button';
+import Slide from '@material-ui/core/Slide';
 import PropTypes from 'prop-types';
+import HelpIcon from 'react-icons/lib/fa/question-circle';
+import IconButton from '@material-ui/core/IconButton';
 
 const styles = theme => ({
     container: {
         position: 'absolute',
-        top: 100,
-        right: 100,
+        bottom: 32,
+        right: 32,
         zIndex: 1000,
-        width: 'min-content'
+        width: 'min-content',
+        [theme.breakpoints.only('xs')]: {
+            // backgroundColor: 'red',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, 60%)'
+        },
     },
     paper: {
-        backgroundColor: theme.palette.primary.main,
+        backgroundColor: theme.palette.secondary.main,
+        color: theme.palette.default,
+        minWidth: 300
     },
     button: {
-        maxWidth: 100,
+        backgroundColor: theme.palette.default,
+        color: theme.palette.secondary.main,
+        minWidth: 150
+    },
+    iconButton: {
+        color: theme.palette.secondary.main,
+        height: 30,
+        width: 30
+    },
+    cardActions: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: 75,
+        minWidth: 200
+    },
+    slideContained: {
+        maxWidth: 150
+    },
+    helpsecondary: {
+        color: theme.palette.secondary.main
+    },
+    helpDefault: {
         color: theme.palette.default,
-    }
+        height: 30,
+        width: 30
+    },
 });
 
 class HelpSuggestion extends Component {
+
+    state = {
+        button: true,
+    }
+
     render() {
         const { classes, toggleHelp, toggleHelpSuggestion, isRehydrated, show, clientType } = this.props;
         if (!isRehydrated || (isRehydrated && !show.helpSuggestion)) {
@@ -33,15 +74,38 @@ class HelpSuggestion extends Component {
             <div className={classes.container} style={isMobie ? { top: "50%", left: '50%', transform: "translate(-50%, -50%)" } : {}}>
                 <Card elevation={1} className={classes.paper}>
                     <CardContent>
-                        <Button
-                            onClick={() => { toggleHelpSuggestion(false); toggleHelp(true) }}
-                            className={classes.button}
-                        >
-                            Comment naviguer dans le graph?
-                        </Button>
+                        Comment naviguer dans le graph?
                     </CardContent>
+                    <CardActions
+                        onMouseEnter={() => { this.setState({ button: false }) }}
+                        onMouseLeave={() => { this.setState({ button: true }) }}
+                        className={classes.cardActions}
+                    >
+
+                        <Slide mountOnEnter unmountOnExit direction="left" in={this.state.button}>
+                            <div className={classes.slideContained}>
+                                <Button
+                                    className={classes.button}
+                                >
+                                    Suivez le guide!
+                                </Button>
+                            </div>
+                        </Slide>
+                        <Slide mountOnEnter unmountOnExit direction="left" in={!this.state.button}>
+                            <div className={classes.slideContained}>
+                                <IconButton
+                                    onClick={() => { toggleHelpSuggestion(false); toggleHelp(true) }}
+                                    className={classes.iconButton}
+                                    color="secondary"
+                                >
+                                    <HelpIcon className={classes.helpDefault} />
+                                </IconButton>
+                            </div>
+                        </Slide>
+
+                    </CardActions>
                 </Card>
-            </div>
+            </div >
         )
     }
 }
